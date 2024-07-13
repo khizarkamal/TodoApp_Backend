8ed71943b18895996f20938eeac5e9edfb8faa24
khizar <khizarkamal0321@gmail.com>
2024-07-13 06:36:40 -0700

    Added multiple controllerd for updating, deleting and creating todos

--- controllers/todoControllers.js
+++ controllers/todoControllers.js
@@ -25,7 +25,7 @@ exports.createTodo = async (req,res) => {
     try{
     const createdTodo = await TodoModel.create(req.body);
     res.json({
-        status:"success",
+        status:201,
         message:"Todo created successfully",
         data: {
           todo:createdTodo  
@@ -35,16 +35,65 @@ exports.createTodo = async (req,res) => {
     catch(error){
         console.log(error);
         res.json({
-            status:"failed",
+            status:400,
             message:"Something went wrong while creating the todo"
         })
     }
 }
 
-exports.updateTodo = async () =>{
-    console.log("Update Todo");
+exports.updateTodo = async (req,res) =>{
+    try{
+        const id = req.params.id;
+        const updatedTodo = await TodoModel.findByIdAndUpdate(id, req.body , {
+            new:true,
+            runValidators:true
+        });
+
+        if(!updatedTodo){
+            return res.json({
+                status:200,
+                message:"No todo found for the given id",
+            })
+        }
+
+        return res.json({
+            status:200,
+            message:"Successfully updated todo",
+            data:{
+                todo: updatedTodo
+            }
+        })
+    }
+    catch(error){
+        console.log("Error", error);
+        res.json({
+            status:400,
+            message:"Something went wrong while updating the todo item"
+        })
+    }
 }
 
-exports.deleteTodos = async () =>{
+exports.deleteTodos = async (req,res) =>{
+    try{
+        const id = req.params.id;
+        const deletedTodo = await TodoModel.findByIdAndDelete(id);
 
+        if(!deletedTodo){
+            return res.json({
+                status:200,
+                message:"No todo found for the given id",
+            })
+        }
+        return res.json({
+            status:200,
+            message:"Successfully deleted todo",
+        })
+    }
+    catch(error){
+        console.log("Error", error);
+        res.json({
+            status:400,
+            message:"Something went wrong while deleting the todo item"
+        })
+    }
 } 
