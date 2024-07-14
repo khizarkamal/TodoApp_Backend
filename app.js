84bfef3d52b060820937987e85a1499fb4ad0b19
khizar <khizarkamal0321@gmail.com>
2024-07-14 03:13:20 -0700

    Added cors for api to be fetched

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require('cors')
 const todoRouter = require("./routes/todoRoutes");
 
 const app = express();
 
+app.use(cors());
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
